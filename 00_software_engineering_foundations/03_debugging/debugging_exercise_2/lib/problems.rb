1e3ae8cfa9c51044bf15d95df17f7dd1fa741ada
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-12 14:53:25 +0800

    Implement #largest_prime_factor

--- 00_software_engineering_foundations/03_debugging/debugging_exercise_2/lib/problems.rb
+++ 00_software_engineering_foundations/03_debugging/debugging_exercise_2/lib/problems.rb
@@ -1,3 +1,23 @@
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
+
+def largest_prime_factor(num)
+  num.downto(2) do |i|
+    if is_prime?(i) && num % i == 0
+      return i
+    end
+  end
+  
+  num
+end
+
+def is_prime?(num)
+  return false if num < 2
+
+  (2...num).each do |i|
+    return false if num % i == 0
+  end
+
+  true
+end
