7bb48fbc213bd573a3fb0c1f2755964b7fe19e73
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-13 19:01:10 +0800

    Part 1, done

--- 00_software_engineering_foundations/08_object_oriented_programming/startup_project/lib/employee.rb
+++ 00_software_engineering_foundations/08_object_oriented_programming/startup_project/lib/employee.rb
@@ -1,3 +1,11 @@
 class Employee
+  attr_reader :name, :title
 
+  def initialize(name, title)
+    @name, @title, @earnings = name, title, 0
+  end
+
+  def pay(amount)
+    @earnings += amount
+  end
 end
