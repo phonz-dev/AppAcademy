4c3c3b4ec72d7f1e16ef6b1c2debd844181732fa
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-13 11:07:06 +0800

    Finish project

--- 00_software_engineering_foundations/05_classes/dog_project/lib/dog.rb
+++ 00_software_engineering_foundations/05_classes/dog_project/lib/dog.rb
@@ -1,3 +1,42 @@
 class Dog
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
 
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def age
+    @age
+  end
+
+  def age=(new_age)
+    @age = new_age
+    @age
+  end
+
+  def bark
+    if @age > 3
+      return @bark.upcase
+    else
+      return @bark.downcase
+    end
+  end
+
+  def favorite_foods
+    @favorite_foods
+  end
+
+  def favorite_food?(food)
+    @favorite_foods.any? { |fav_food| fav_food == food.capitalize }
+  end
 end
