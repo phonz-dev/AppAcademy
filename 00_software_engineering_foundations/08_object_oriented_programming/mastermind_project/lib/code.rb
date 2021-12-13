aeeb62899477c31ea3571f28b2faef272c8b39b5
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-13 20:04:59 +0800

    Part 1, done

--- 00_software_engineering_foundations/08_object_oriented_programming/mastermind_project/lib/code.rb
+++ 00_software_engineering_foundations/08_object_oriented_programming/mastermind_project/lib/code.rb
@@ -6,4 +6,35 @@ class Code
     "Y" => :yellow
   }
 
+  attr_reader :pegs
+
+  def self.valid_pegs?(pegs)
+    valid_pegs = POSSIBLE_PEGS.keys
+    pegs.map(&:capitalize)
+        .all? { |peg| valid_pegs.include?(peg) }
+  end
+
+  def self.random(length)
+    valid_pegs = POSSIBLE_PEGS.keys
+    new_pegs = []
+    length.times { new_pegs << valid_pegs.sample }
+    Code.new(new_pegs)
+  end
+
+  def self.from_string(str_pegs)
+    Code.new(str_pegs.split(""))
+  end
+
+  def initialize(pegs)
+    raise "invalid pegs" unless Code.valid_pegs?(pegs)
+    @pegs = pegs.map(&:capitalize)
+  end
+
+  def [](index)
+    @pegs[index]
+  end
+
+  def length
+    @pegs.length
+  end
 end
