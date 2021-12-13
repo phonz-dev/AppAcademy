c7eeecf1ecdf6ddafe0b89856da62a35af9be6ec
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-13 19:24:30 +0800

    Part 2, done

--- 00_software_engineering_foundations/08_object_oriented_programming/startup_project/lib/startup.rb
+++ 00_software_engineering_foundations/08_object_oriented_programming/startup_project/lib/startup.rb
@@ -1,5 +1,48 @@
 require "employee"
 
 class Startup
+  attr_reader :name, :funding, :salaries, :employees
 
+  def initialize(name, funding, salaries)
+    @name = name
+    @funding = funding
+    @salaries = salaries
+    @employees = []
+  end
+
+  def valid_title?(title)
+    @salaries.has_key?(title)
+  end
+
+  def >(other_startup)
+    @funding > other_startup.funding
+  end
+
+  def hire(employee_name, title)
+    unless self.valid_title?(title)
+      raise "invalid title"
+    end
+
+    new_employee = Employee.new(employee_name, title)
+    @employees << new_employee
+  end
+
+  def size
+    @employees.length
+  end
+
+  def pay_employee(employee)
+    employee_salary = @salaries[employee.title]
+
+    if @funding < employee_salary
+      raise "insufficient funds"
+    end
+
+    employee.pay(employee_salary)
+    @funding -= employee_salary
+  end
+
+  def payday
+    @employees.each { |employee| self.pay_employee(employee) }
+  end
 end
