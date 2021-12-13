18356dc1f8266372d19bb7381d6230facbf88b92
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-13 16:57:57 +0800

    Create Room class

--- 00_software_engineering_foundations/07_input_output/hotel_project/lib/room.rb
+++ 00_software_engineering_foundations/07_input_output/hotel_project/lib/room.rb
@@ -1,3 +1,28 @@
 class Room
-  
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    !(@occupants.length < @capacity)
+  end
+
+  def available_space
+    @capacity - @occupants.length
+  end
+
+  def add_occupant(new_occupant)
+    return false if self.full?
+    @occupants << new_occupant
+    true
+  end
 end
