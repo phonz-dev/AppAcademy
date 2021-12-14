6a182e88696174ea35c53ae6c7a231d266b166fc
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-14 19:25:01 +0800

    Implement #vigenere_cipher

--- 00_software_engineering_foundations/10_foundations_recap/recap_exercise_3/problems.rb
+++ 00_software_engineering_foundations/10_foundations_recap/recap_exercise_3/problems.rb
@@ -131,3 +131,43 @@ end
 # p bi_prime?(94)   # => true
 # p bi_prime?(24)   # => false
 # p bi_prime?(64)   # => false
+
+#####################################################################
+
+# vigenere_cipher
+# A Caesar cipher takes a word and encrypts it by offsetting each letter 
+# in the word by a fixed number, called the key. Given a key of 3, for 
+# example: a -> d, p -> s, and y -> b.
+
+# A Vigenere Cipher is a Caesar cipher, but instead of a single key, a 
+# sequence of keys is used. For example, if we encrypt "bananasinpajamas" 
+# with the key sequence 1, 2, 3, then the result would be "ccqbpdtkqqcmbodt":
+
+# Message:  b a n a n a s i n p a j a m a s
+# Keys:     1 2 3 1 2 3 1 2 3 1 2 3 1 2 3 1
+# Result:   c c q b p d t k q q c m b o d t
+# Write a method vigenere_cipher(message, keys) that accepts a string and a 
+# key-sequence as args, returning the encrypted message. Assume that the message 
+# consists of only lowercase alphabetic characters.
+
+def vigenere_cipher(message, keys)
+  alphabet = ("a".."z").to_a
+  new_message = ""
+  
+  message.each_char.with_index do |char, i|
+    old_idx = alphabet.index(char)
+    new_idx = keys[i % keys.length] + old_idx 
+    new_char = alphabet[new_idx % alphabet.length]
+
+    new_message += new_char
+  end
+
+  new_message
+end
+
+# Examples
+# p vigenere_cipher("toerrishuman", [1])        # => "upfssjtivnbo"
+# p vigenere_cipher("toerrishuman", [1, 2])     # => "uqftsktjvobp"
+# p vigenere_cipher("toerrishuman", [1, 2, 3])  # => "uqhstltjxncq"
+# p vigenere_cipher("zebra", [3, 0])            # => "ceerd"
+# p vigenere_cipher("yawn", [5, 1])             # => "dbbo"
