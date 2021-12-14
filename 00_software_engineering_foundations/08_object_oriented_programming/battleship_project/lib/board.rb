906a9f868069da0a4cf6960a20993a11166b34b6
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-14 10:54:17 +0800

    Add Board class

--- 00_software_engineering_foundations/08_object_oriented_programming/battleship_project/lib/board.rb
+++ 00_software_engineering_foundations/08_object_oriented_programming/battleship_project/lib/board.rb
@@ -1,4 +1,8 @@
 class Board
+  def self.print_grid(grid)
+    grid.each { |row| puts row.join(" ") }
+  end
+
   attr_reader :size
 
   def initialize(n)
@@ -18,5 +22,44 @@ class Board
 
   def num_ships
     @grid.flatten.count { |val| val == :S } 
+  end
+
+  def attack(pos)
+    if self[pos] == :S
+      self[pos] = :H
+      puts "you sunk my battleship!"
+      return true
+    else
+      self[pos] = :X
+      return false
+    end
+  end
+
+  def place_random_ships
+    ship_count = (@size * 0.25).to_i 
+    rand_positions = self.board_positions.sample(ship_count)
+    rand_positions.each { |pos| self[pos] = :S }
+  end
+
+  def board_positions
+    positions = []
+    (0...@grid.length).each do |row|
+      (0...@grid.length).each do |col|
+        positions << [row, col]
+      end
+    end
+    positions
+  end
+
+  def hidden_ships_grid
+    @grid.map { |row| row.map { |el| el == :S ? :N : el} }
+  end
+
+  def cheat
+    Board.print_grid(@grid)
+  end
+
+  def print
+    Board.print_grid(self.hidden_ships_grid)
   end
 end
