5b92a08d503e3638cb3e6841ae88e86788c024d2
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-14 19:47:43 +0800

    Implement String#select

--- 00_software_engineering_foundations/10_foundations_recap/recap_exercise_3/problems.rb
+++ 00_software_engineering_foundations/10_foundations_recap/recap_exercise_3/problems.rb
@@ -205,3 +205,28 @@ end
 # p vowel_rotate('headphones')    # => "heedphanos"
 # p vowel_rotate('bootcamp')      # => "baotcomp"
 # p vowel_rotate('awesome')       # => "ewasemo"
+
+#####################################################################
+
+# PROC PROBLEMS
+
+# String#select
+# Extend the string class by defining a String#select method that accepts 
+# a block. The method should return a new string containing characters of 
+# the original string that return true when passed into the block. If no 
+# block is passed, then return the empty string. Do not use the built-in 
+# Array#select in your solution.
+
+class String
+  def select(&prc)
+    return "" if prc.nil?
+    new_str = ""
+    self.each_char { |ch| new_str += ch if prc.call(ch) }
+    new_str
+  end
+end
+
+# Examples
+# p "app academy".select { |ch| !"aeiou".include?(ch) }   # => "pp cdmy"
+# p "HELLOworld".select { |ch| ch == ch.upcase }          # => "HELLO"
+# p "HELLOworld".select          # => ""
