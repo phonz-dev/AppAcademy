33e5ecea5bd2c85c8a743f9bd21a6ed3ed34578b
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-14 17:50:28 +0800

    Finish problems

--- 00_software_engineering_foundations/10_foundations_recap/recap_exercise_2/lib/problems.rb
+++ 00_software_engineering_foundations/10_foundations_recap/recap_exercise_2/lib/problems.rb
@@ -1,21 +1,34 @@
 # Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
 # of both of the given numbers
 def least_common_multiple(num_1, num_2)
-
+    (1..num_1 * num_2).each do |dividend|
+        if dividend % num_1 == 0 && dividend % num_2 == 0
+            return dividend
+        end
+    end
 end
 
 
 # Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
 # most in the string.
 def most_frequent_bigram(str)
+    bigram_counts = Hash.new(0)
 
+    (0...str.length - 1).each do |i|
+        bigram = str[i] + str[i + 1]
+        bigram_counts[bigram] += 1
+    end
+
+    sorted = bigram_counts.sort_by { |k, v| v }
+    sorted.last.first
 end
-
 
 class Hash
     # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
-
+        inversed = {}
+        self.each { |k, v| inversed[v] = k }
+        inversed
     end
 end
 
@@ -23,7 +36,16 @@ end
 class Array
     # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
     def pair_sum_count(num)
+        count = 0
 
+        (0...self.length).each do |start_idx|
+            (start_idx + 1...self.length).each do |end_idx|
+                sum = self[start_idx] + self[end_idx]
+                count += 1 if sum == num
+            end
+        end
+
+        count
     end
 
     # Write a method, Array#bubble_sort, that takes in an optional proc argument.
@@ -40,6 +62,21 @@ class Array
     #
     # This should remind you of the spaceship operator! Convenient :)
     def bubble_sort(&prc)
+        prc ||= Proc.new { |a, b| a <=> b }
 
+        not_sorted = true
+        
+        while not_sorted
+            not_sorted = false
+
+            (0...self.length - 1).each do |i|
+                if prc.call(self[i], self[i + 1]) == 1
+                    self[i], self[i + 1] = self[i + 1], self[i]
+                    not_sorted = true
+                end
+            end
+        end
+
+        self
     end
 end
