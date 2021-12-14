a8b7bda7f28019676bbcfadac294c02610df8ba6
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-14 18:11:29 +0800

    Implement #char_indices

--- 00_software_engineering_foundations/10_foundations_recap/recap_exercise_3/problems.rb
+++ 00_software_engineering_foundations/10_foundations_recap/recap_exercise_3/problems.rb
@@ -36,3 +36,21 @@ end
 # p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
 # p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
 # p no_consecutive_repeats?(['x'])                              # => true
+
+#####################################################################
+
+# char_indices
+# Write a method char_indices(str) that takes in a string as an arg. The 
+# method should return a hash containing characters as keys. The value 
+# associated with each key should be an array containing the indices where 
+# that character is found.
+
+def char_indices(str)
+  indices = Hash.new { |h, k| h[k] = [] }
+  str.each_char.with_index { |ch, i| indices[ch] << i }
+  indices
+end
+
+# Examples
+# p char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
+# p char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
