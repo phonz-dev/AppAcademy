8683ef9a01c6b82eb8b7bd0ea98d97bb1048bd67
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-14 09:04:00 +0800

    Part 2, done

--- 00_software_engineering_foundations/08_object_oriented_programming/mastermind_project/lib/code.rb
+++ 00_software_engineering_foundations/08_object_oriented_programming/mastermind_project/lib/code.rb
@@ -37,4 +37,47 @@ class Code
   def length
     @pegs.length
   end
+
+  def num_exact_matches(guess)
+    match_count = 0
+    @pegs.each_with_index do |peg, i|
+      match_count += 1 if peg == guess[i]
+    end
+    match_count
+  end
+
+  def num_near_matches(guess)
+    secret_pegs, guess_pegs = remove_exact_matches(@pegs, guess.pegs)
+
+    near_match_count = 0
+    seen = []
+
+    secret_pegs.each do |peg|
+      if guess_pegs.include?(peg) && !seen.include?(peg)
+        near_match_count += 1
+        seen << peg
+      end
+    end
+
+    near_match_count
+  end
+
+  def remove_exact_matches(pegs_1, pegs_2)
+    pegs_1_dup = pegs_1.dup
+    pegs_2_dup = pegs_2.dup
+
+    pegs_1_dup.each_with_index do |peg, i|
+      if peg == pegs_2_dup[i]
+        pegs_1_dup[i] = nil
+        pegs_2_dup[i] = nil
+      end
+    end
+
+    [pegs_1_dup.compact, pegs_2_dup.compact]
+  end
+
+  def ==(code)
+    @pegs == code.pegs
+  end
 end
+
