043f2313701c9bae8632316aed9a665703d42b0e
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-14 10:12:40 +0800

    Part 1, done

--- 00_software_engineering_foundations/08_object_oriented_programming/battleship_project/lib/board.rb
+++ 00_software_engineering_foundations/08_object_oriented_programming/battleship_project/lib/board.rb
@@ -1,3 +1,22 @@
 class Board
-  
+  attr_reader :size
+
+  def initialize(n)
+    @grid = Array.new(n) { Array.new(n).fill(:N)}
+    @size = n * n
+  end
+
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos, val)
+    row, col = pos
+    @grid[row][col] = val
+  end
+
+  def num_ships
+    @grid.flatten.count { |val| val == :S } 
+  end
 end
