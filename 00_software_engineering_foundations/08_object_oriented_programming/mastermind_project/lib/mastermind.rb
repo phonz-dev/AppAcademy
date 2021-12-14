0c6d6d06b14630f1c80838acb162bc103a9a9695
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-14 09:29:38 +0800

    Part 3, done

--- 00_software_engineering_foundations/08_object_oriented_programming/mastermind_project/lib/mastermind.rb
+++ 00_software_engineering_foundations/08_object_oriented_programming/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,22 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  def print_matches(code)
+    exact_matches = @secret_code.num_exact_matches(code)
+    near_matches = @secret_code.num_near_matches(code)
+    puts "Exact matches: #{exact_matches}"
+    puts "Near matches: #{near_matches}"
+  end
+
+  def ask_user_for_guess
+    puts "Enter a code"
+    input = gets.chomp
+    code = Code.from_string(input)
+    self.print_matches(code)
+    @secret_code == code
+  end
 end
