b655827fbe01db46ae0fe99555bf864191954d6a
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-14 19:55:13 +0800

    Implement String#map!

--- 00_software_engineering_foundations/10_foundations_recap/recap_exercise_3/problems.rb
+++ 00_software_engineering_foundations/10_foundations_recap/recap_exercise_3/problems.rb
@@ -207,6 +207,8 @@ end
 # p vowel_rotate('awesome')       # => "ewasemo"
 
 #####################################################################
+#####################################################################
+
 
 # PROC PROBLEMS
 
@@ -230,3 +232,44 @@ end
 # p "app academy".select { |ch| !"aeiou".include?(ch) }   # => "pp cdmy"
 # p "HELLOworld".select { |ch| ch == ch.upcase }          # => "HELLO"
 # p "HELLOworld".select          # => ""
+
+#####################################################################
+
+# String#map!
+# Extend the string class by defining a String#map! method that accepts a block. 
+# The method should modify the existing string by replacing every character with 
+# the result of calling the block, passing in the original character and it's index. 
+# Do not use the built-in Array#map or Array#map! in your solution.
+
+class String
+  def map!(&prc)
+    self.each_char.with_index do |ch, i|
+      if prc.call(ch, i)
+        self[i] = prc.call(ch, i)
+      end
+    end
+  end
+end
+
+# Examples
+# word_1 = "Lovelace"
+# word_1.map! do |ch| 
+#     if ch == 'e'
+#         '3'
+#     elsif ch == 'a'
+#         '4'
+#     else
+#         ch
+#     end
+# end
+# p word_1        # => "Lov3l4c3"
+
+# word_2 = "Dijkstra"
+# word_2.map! do |ch, i|
+#     if i.even?
+#         ch.upcase
+#     else
+#         ch.downcase
+#     end
+# end
+# p word_2        # => "DiJkStRa"
