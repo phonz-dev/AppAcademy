f406015875e887eceb9363e6f67e63ffcd6adfe7
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-15 17:40:40 +0800

    Implement #pair_product

--- 00_software_engineering_foundations/11_additional_projects/nauseating_numbers/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/nauseating_numbers/problems.rb
@@ -29,3 +29,33 @@ end
 # p strange_sums([9])                   # 0
 
 ##################################################################################
+
+# pair_product
+# Write a method pair_product that accepts an array of numbers and a product as arguments. 
+# The method should return a boolean indicating whether or not a pair of distinct elements 
+# in the array result in the product when multiplied together. You may assume that the input 
+# array contains unique elements.
+
+def pair_product(nums, product)
+  (0...nums.length).each do |idx_1|
+    (idx_1 + 1...nums.length).each do |idx_2|
+      num_1, num_2 = nums[idx_1], nums[idx_2]
+      return true if num_1 * num_2 == product
+    end
+  end
+
+  false
+end
+
+# Examples
+
+# p pair_product([4, 2, 5, 8], 16)    # true
+# p pair_product([8, 1, 9, 3], 8)     # true
+# p pair_product([3, 4], 12)          # true
+# p pair_product([3, 4, 6, 2, 5], 12) # true
+# p pair_product([4, 2, 5, 7], 16)    # false
+# p pair_product([8, 4, 9, 3], 8)     # false
+# p pair_product([3], 12)             # false
+
+##################################################################################
+
