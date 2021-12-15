55019821692787f25d47579119cb64c6caca20a1
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-15 20:54:50 +0800

    Implement #triangular_word?

--- 00_software_engineering_foundations/11_additional_projects/nauseating_numbers/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/nauseating_numbers/problems.rb
@@ -495,3 +495,48 @@ end
 # p mersenne_prime(3) # 31
 # p mersenne_prime(4) # 127
 # p mersenne_prime(6) # 131071
+
+##################################################################################
+
+# triangular_word?
+# A triangular number is a number of the form (i * (i + 1)) / 2 where i is some positive 
+# integer. Substituting i with increasing integers gives the triangular number sequence. 
+# The first five numbers of the triangular number sequence are 1, 3, 6, 10, 15.
+
+# We can encode a word as a number by taking the sum of its letters based on their position 
+# in the alphabet. For example, we can encode "cat" as 24 because c is the 3rd of the alphabet, 
+# a is the 1st, and t is the 20th:
+
+# 3 + 1 + 20 = 24
+
+# Write a method triangular_word? that accepts a word as an argument and returns a boolean 
+# indicating whether or not that word's number encoding is a triangular number. You can assume 
+# that the argument contains lowercase letters.
+
+
+def triangular_word?(word)
+  alphabet = ("a".."z").to_a
+  encoding_num = word.split("").map { |ch| alphabet.index(ch) + 1 }.sum
+
+  (1...encoding_num).each do |i|
+    triangular_num = (i * (i + 1)) / 2
+    return true if encoding_num == triangular_num
+  end
+
+  false
+end
+
+
+# Examples
+
+# p triangular_word?('abc')       # true
+# p triangular_word?('ba')        # true
+# p triangular_word?('lovely')    # true
+# p triangular_word?('question')  # true
+# p triangular_word?('aa')        # false
+# p triangular_word?('cd')        # false
+# p triangular_word?('cat')       # false
+# p triangular_word?('sink')      # false
+
+##################################################################################
+
