fdfc079a7b38ec2b8cf23a2b1ca3af338b59c734
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-15 18:39:29 +0800

    Implement #anti_prime?

--- 00_software_engineering_foundations/11_additional_projects/nauseating_numbers/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/nauseating_numbers/problems.rb
@@ -113,3 +113,37 @@ end
 # p perfect_square?(50)    # false
 
 ##################################################################################
+##################################################################################
+
+# PHASE 2
+
+# anti_prime?
+# Write a method anti_prime? that accepts a number as an argument. The method should 
+# return true if the given number has more divisors than all positive numbers less than 
+# the given number. For example, 24 is an anti-prime because it has more divisors than 
+# any positive number less than 24. Math Fact: Numbers that meet this criteria are also 
+# known as highly composite numbers.
+
+def anti_prime?(num)
+  num_divisors_count = count_divisors(num)
+  (1...num).none? { |other_num| count_divisors(other_num) > num_divisors_count }
+end
+
+def count_divisors(num)
+  (1..num).count { |divisor| num % divisor == 0 }
+end
+
+# Examples
+
+# p anti_prime?(24)   # true
+# p anti_prime?(36)   # true
+# p anti_prime?(48)   # true
+# p anti_prime?(360)  # true
+# p anti_prime?(1260) # true
+# p anti_prime?(27)   # false
+# p anti_prime?(5)    # false
+# p anti_prime?(100)  # false
+# p anti_prime?(136)  # false
+# p anti_prime?(1024) # false
+
+##################################################################################
