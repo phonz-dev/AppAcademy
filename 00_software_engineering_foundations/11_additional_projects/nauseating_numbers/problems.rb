f8cdea8b197a3a97aade4129f3213c2f39d46aa9
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-15 19:11:17 +0800

    Implement #mutual_factors

--- 00_software_engineering_foundations/11_additional_projects/nauseating_numbers/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/nauseating_numbers/problems.rb
@@ -182,3 +182,31 @@ end
 # p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
 # p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
 # p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
+
+##################################################################################
+
+# mutual_factors
+# Write a method mutual_factors that accepts any amount of numbers as arguments. The 
+# method should return an array containing all of the common divisors shared among the 
+# arguments. For example, the common divisors of 50 and 30 are 1, 2, 5, 10. You can assume 
+# that all of the arguments are positive integers.
+
+def mutual_factors(*nums)
+  (1..nums.min).select do |divisor|
+    factors_count = nums.count { |num| num % divisor == 0 }
+    factors_count == nums.length
+  end
+end
+
+# Examples
+
+# p mutual_factors(50, 30)            # [1, 2, 5, 10]
+# p mutual_factors(50, 30, 45, 105)   # [1, 5]
+# p mutual_factors(8, 4)              # [1, 2, 4]
+# p mutual_factors(8, 4, 10)          # [1, 2]
+# p mutual_factors(12, 24)            # [1, 2, 3, 4, 6, 12]
+# p mutual_factors(12, 24, 64)        # [1, 2, 4]
+# p mutual_factors(22, 44)            # [1, 2, 11, 22]
+# p mutual_factors(22, 44, 11)        # [1, 11]
+# p mutual_factors(7)                 # [1, 7]
+# p mutual_factors(7, 9)              # [1]
