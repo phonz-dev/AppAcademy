f12df96484e8d62969ef2ece227a2a747f7a2619
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-15 19:42:08 +0800

    Implement #squarocol?

--- 00_software_engineering_foundations/11_additional_projects/nauseating_numbers/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/nauseating_numbers/problems.rb
@@ -266,7 +266,6 @@ def matrix_addition_reloaded(*matrices)
   matrices.inject { |m_1, m_2| matrix_addition(m_1, m_2) }
 end
 
-
 # Examples
 
 # matrix_a = [[2,5], [4,7]]
@@ -284,3 +283,52 @@ end
 
 ##################################################################################
 
+# squarocol?
+# Write a method squarocol? that accepts a 2-dimensional array as an argument. The method 
+# should return a boolean indicating whether or not any row or column is completely filled 
+# with the same element. You may assume that the 2-dimensional array has "square" dimensions, 
+# meaning it's height is the same as it's width.
+
+def squarocol?(array)
+  transposed = array.transpose
+  row_complete = array.any? { |row| row.uniq.length == 1 }
+  col_complete = transposed.any? { |col| col.uniq.length == 1 }
+
+  row_complete || col_complete
+end
+
+# Examples
+
+# p squarocol?([
+#     [:a, :x , :d],
+#     [:b, :x , :e],
+#     [:c, :x , :f],
+# ]) # true
+
+# p squarocol?([
+#     [:x, :y, :x],
+#     [:x, :z, :x],
+#     [:o, :o, :o],
+# ]) # true
+
+# p squarocol?([
+#     [:o, :x , :o],
+#     [:x, :o , :x],
+#     [:o, :x , :o],
+# ]) # false
+
+# p squarocol?([
+#     [1, 2, 2, 7],
+#     [1, 6, 6, 7],
+#     [0, 5, 2, 7],
+#     [4, 2, 9, 7],
+# ]) # true
+
+# p squarocol?([
+#     [1, 2, 2, 7],
+#     [1, 6, 6, 0],
+#     [0, 5, 2, 7],
+#     [4, 2, 9, 7],
+# ]) # false
+
+##################################################################################
