996bf4c552a835de21bb372abbd0b0106ed4dda2
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 17:53:20 +0800

    Implement #disjunct_select

--- 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
@@ -353,3 +353,33 @@ end
 # p reverberate('Her family flew to France') # "Herer familyily flewew to Francefrance"
 
 ##########################################################################################
+
+# disjunct_select
+# Write a method disjunct_select that accepts an array and one or more procs as arguments. 
+# The method should return a new array containing the elements that return true when passed 
+# into at least one of the given procs.
+
+def disjunct_select(arr, *procs)
+  arr.select { |el| procs.any? { |prc| prc.call(el) } }
+end
+
+# Examples
+
+longer_four = Proc.new { |s| s.length > 4 }
+contains_o = Proc.new { |s| s.include?('o') }
+starts_a = Proc.new { |s| s[0] == 'a' }
+
+p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+    longer_four,
+) # ["apple", "teeming"]
+
+p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+    longer_four,
+    contains_o
+) # ["dog", "apple", "teeming", "boot"]
+
+p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+    longer_four,
+    contains_o,
+    starts_a
+) # ["ace", "dog", "apple", "teeming", "boot"]  
