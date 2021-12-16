b32e0c3cf1890618b7f92b8a7eae16ad29824680
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 16:56:03 +0800

    Implement #hash_mapped

--- 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
@@ -53,3 +53,29 @@ end
 
 ##########################################################################################
 
+# hash_mapped
+# Write a method hash_mapped that accepts a hash, a proc, and a block. The method should 
+# return a new hash where each key is the result of the original key when given to the block. 
+# Each value of the new hash should be the result of the original values when passed into the proc.
+
+def hash_mapped(hash, prc, &blk)
+  new_hash = {}
+
+  hash.each do |key, value|
+    new_key = blk.call(key)
+    new_value = prc.call(value)
+    new_hash[new_key] = new_value
+  end
+
+  new_hash
+end
+
+# Examples
+
+# double = Proc.new { |n| n * 2 }
+# p hash_mapped({'a'=>4, 'x'=>7, 'c'=>-3}, double) { |k| k.upcase + '!!' }
+# # {"A!!"=>8, "X!!"=>14, "C!!"=>-6}
+
+# first = Proc.new { |a| a[0] }
+# p hash_mapped({-5=>['q', 'r', 's'], 6=>['w', 'x']}, first) { |n| n * n }
+# # {25=>"q", 36=>"w"}
