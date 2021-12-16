80ba2491c1ef2235bf8afcbbb4fd8863fffa0199
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 14:57:24 +0800

    Implement #proc_suffix

--- 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
@@ -349,3 +349,60 @@ end
 # p chain_map(25, [add_5, half, square])  # 225
 # p chain_map(4, [square, half])          # 8
 # p chain_map(4, [half, square])          # 4
+
+#####################################################################################
+
+# proc_suffix
+# Write a method proc_suffix that accepts a sentence and a hash as arguments. The hash 
+# contains procs as keys and suffix strings as values. The method should return a new 
+# sentence where each word of the original sentence is appended with a suffix if the original 
+# word returns true when given to the corresponding proc key. If an original word returns true 
+# for multiple procs, then the suffixes should be appended in the order that they appear in the 
+# input hash.
+
+def proc_suffix(sentence, hash)
+  words = sentence.split
+  new_words = []
+
+  words.each do |word|
+    new_word = word
+
+    hash.each do |prc, suffix|
+      if prc.call(word)
+        new_word += suffix
+      end
+    end
+
+    new_words << new_word
+  end
+
+  new_words.join(" ")
+end
+
+# Examples
+
+# contains_a = Proc.new { |w| w.include?('a') }
+# three_letters = Proc.new { |w| w.length == 3 }
+# four_letters = Proc.new { |w| w.length == 4 }
+
+# p proc_suffix('dog cat',
+#     contains_a => 'ly',
+#     three_letters => 'o'
+# )   # "dogo catlyo"
+
+# p proc_suffix('dog cat',
+#     three_letters => 'o',
+#     contains_a => 'ly'
+# )   # "dogo catoly"
+
+# p proc_suffix('wrong glad cat',
+#     contains_a => 'ly',
+#     three_letters => 'o',
+#     four_letters => 'ing'
+# )   # "wrong gladlying catlyo"
+
+# p proc_suffix('food glad rant dog cat',
+#     four_letters => 'ing',
+#     contains_a => 'ly',
+#     three_letters => 'o'
+# )   # "fooding gladingly rantingly dogo catlyo"
