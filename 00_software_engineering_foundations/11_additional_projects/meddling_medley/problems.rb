539474548caaa170b646407893aa4ba482ac93ff
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 16:51:03 +0800

    Implement #sentence_swap

--- 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
@@ -17,3 +17,39 @@ end
 # p duos('e')             # 0
 
 ##########################################################################################
+
+# sentence_swap
+# Write a method sentence_swap that accepts a sentence and a hash as arguments. The method 
+# should return a new sentence where every word is replaced with it's corresponding value in 
+# the hash. If a word does not exist as a key of the hash, then it should remain unchanged.
+
+def sentence_swap(sentence, swapper)
+  words = sentence.split(" ")
+
+  new_words = words.map do |word|
+    if swapper.has_key?(word)
+      swapper[word]
+    else
+      word
+    end
+  end
+  
+  new_words.join(" ")
+end
+
+# Examples
+
+# p sentence_swap('anything you can do I can do',
+#     'anything'=>'nothing', 'do'=>'drink', 'can'=>'shall'
+# ) # 'nothing you shall drink I shall drink'
+
+# p sentence_swap('what a sad ad',
+#     'cat'=>'dog', 'sad'=>'happy', 'what'=>'make'
+# ) # 'make a happy ad'
+
+# p sentence_swap('keep coding okay',
+#     'coding'=>'running', 'kay'=>'pen'
+# ) # 'keep running okay'
+
+##########################################################################################
+
