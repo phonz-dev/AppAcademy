3b3f431c2e429ef76fb25e2ea43d131dce99a59b
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 13:57:58 +0800

    Implement #xnor_select

--- 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
@@ -166,3 +166,28 @@ end
 # p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
 # p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
 # p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }            # nil
+
+#####################################################################################
+#####################################################################################
+
+# PHASE 2
+
+# xnor_select
+# Write a method xnor_select that accepts an array and two procs as arguments. The method 
+# should return a new array containing elements of the original array that either return 
+# true for both procs or return false for both procs.
+
+def xnor_select(arr, prc_1, prc_2)
+  arr.select do |el|
+    prc_1.call(el) && prc_2.call(el) || !prc_1.call(el) && !prc_2.call(el)
+  end
+end
+
+# Examples
+
+# is_even = Proc.new { |n| n % 2 == 0 }
+# is_odd = Proc.new { |n| n % 2 != 0 }
+# is_positive = Proc.new { |n| n > 0 }
+# p xnor_select([8, 3, -4, -5], is_even, is_positive)         # [8, -5]
+# p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive)  # [-7, -13, 12]
+# p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)   # [5, -10]
