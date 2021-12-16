31bb52cc4de68a395042396cbe7e4e97ecab7159
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 10:00:59 +0800

    Implement #exactly?

--- 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
@@ -18,3 +18,30 @@ end
 # p some?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' } # false
 # p some?(['squash', 'corn', 'kale', 'potato']) { |str| str[0] == 'p' } # true
 # p some?(['parsnip', 'lettuce', 'pea']) { |str| str[0] == 'p' }        # true
+
+#####################################################################################
+
+# exactly?
+# Write a method exactly? that accepts an array, a number (n), and a block as arguments. 
+# The method should return a boolean indicating whether or not there are exactly n elements 
+# that return true when given to the block. Solve this using Array#each.
+
+def exactly?(arr, n, &prc)
+  true_count = 0
+  arr.each { |el| true_count += 1 if prc.call(el) }
+  true_count == n
+end
+
+# Examples
+
+# p exactly?(['A', 'b', 'C'], 2) { |el| el == el.upcase }         # true
+# p exactly?(['A', 'B', 'C'], 2) { |el| el == el.upcase }         # false
+# p exactly?(['A', 'B', 'C'], 3) { |el| el == el.upcase }         # true
+# p exactly?(['cat', 'DOG', 'bird'], 1) { |el| el == el.upcase }  # true
+# p exactly?(['cat', 'DOG', 'bird'], 0) { |el| el == el.upcase }  # false
+# p exactly?(['cat', 'dog', 'bird'], 0) { |el| el == el.upcase }  # true
+# p exactly?([4, 5], 3) { |n| n > 0 }                             # false
+# p exactly?([4, 5, 2], 3) { |n| n > 0 }                          # true
+# p exactly?([42, -9, 7, -3, -6], 2) { |n| n > 0 }                # true
+
+#####################################################################################
