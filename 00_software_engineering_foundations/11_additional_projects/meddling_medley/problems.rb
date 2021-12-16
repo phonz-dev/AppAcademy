3d0b722ca2b3b0cd50d88671798c5d09c446f2fd
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 17:07:43 +0800

    Implement #energetic_encoding

--- 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
@@ -126,3 +126,43 @@ end
 # p triplet_true?('runninggg')     # true
 # p triplet_true?('bootcamp')      # false
 # p triplet_true?('e')             # false
+
+##########################################################################################
+
+# energetic_encoding
+# Write a method energetic_encoding that accepts a string and a hash as arguments. The method 
+# should return a new string where characters of the original string are replaced with the 
+# corresponding values in the hash. If a character is not a key of the hash, then it should be 
+# replaced with a question mark ('?'). Space characters (' ') should remain unchanged.
+
+def energetic_encoding(str, hash)
+  new_chars = str.split("").map do |ch|
+    if ch == " "
+      " "
+    elsif hash.has_key?(ch)
+      hash[ch]
+    else
+      "?"
+    end
+  end
+
+  new_chars.join("")
+end
+
+# Examples
+
+# p energetic_encoding('sent sea',
+#     'e'=>'i', 's'=>'z', 'n'=>'m', 't'=>'p', 'a'=>'u'
+# ) # 'zimp ziu'
+
+# p energetic_encoding('cat',
+#     'a'=>'o', 'c'=>'k'
+# ) # 'ko?'
+
+# p energetic_encoding('hello world',
+#     'o'=>'i', 'l'=>'r', 'e'=>'a'
+# ) # '?arri ?i?r?'
+
+# p energetic_encoding('bike', {}) # '????'
+
+##########################################################################################
