0f2d8997623e3ed38091283745c7669fa09749e7
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 17:36:08 +0800

    Implement #convert_pig_latin

--- 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
@@ -219,3 +219,69 @@ end
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
 
+##########################################################################################
+
+
+# convert_pig_latin
+# Write a method convert_pig_latin that accepts a sentence as an argument. The method should 
+# translate the sentence according to the following rules:
+
+# words that are shorter than 3 characters are unchanged
+# words that are 3 characters or longer are translated according to the following rules:
+  # if the word begins with a vowel, simply add 'yay' to the end of the word (example: 'eat'->'eatyay')
+  # if the word begins with a non-vowel, move all letters that come before the word's first vowel to the 
+  # end of the word and add 'ay' (example: 'trash'->'ashtray')
+
+# Note that if words are capitalized in the original sentence, they should remain capitalized in the 
+# translated sentence. Vowels are the letters a, e, i, o, u.
+
+def append_yay(word)
+  word + "yay"
+end
+
+def append_ay(word)
+  vowels = "aeiou"
+
+  (0...word.length).each do |i|
+    if vowels.include?(word[i])
+      new_word = word[i..-1] + word[0...i] + "ay"
+
+      if word == word.capitalize
+        return new_word.capitalize
+      else
+        return new_word
+      end
+    end
+  end
+  
+  word
+end
+
+def begins_with_vowel?(word)
+  vowels = "aeiou"
+  vowels.include?(word[0].downcase)
+end
+
+def convert_pig_latin(sentence)
+  words = sentence.split(" ")
+
+  new_words = words.map do |word|
+    if word.length <= 2
+      word
+    elsif begins_with_vowel?(word)
+      append_yay(word)
+    else
+      append_ay(word)
+    end
+  end
+
+  new_words.join(" ")
+end
+
+# Examples
+
+# p convert_pig_latin('We like to eat bananas') # "We ikelay to eatyay ananasbay"
+# p convert_pig_latin('I cannot find the trash') # "I annotcay indfay ethay ashtray"
+# p convert_pig_latin('What an interesting problem') # "Atwhay an interestingyay oblempray"
+# p convert_pig_latin('Her family flew to France') # "Erhay amilyfay ewflay to Ancefray"
+# p convert_pig_latin('Our family flew to France') # "Ouryay amilyfay ewflay to Ancefray" 
