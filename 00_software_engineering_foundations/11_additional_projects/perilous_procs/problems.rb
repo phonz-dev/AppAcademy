faa5690e4c1ee0e3f02c5e692522c9c610a96432
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 10:09:36 +0800

    Implement #every?

--- 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
@@ -102,3 +102,24 @@ end
 
 #####################################################################################
 
+# every?
+# Write a method every? that accepts an array and a block as arguments. The method should 
+# return a boolean indicating whether or not all elements of the array return true when given 
+# to the block. Solve this using Array#each.
+
+def every?(arr, &prc)
+  arr.each { |el| return false unless prc.call(el) }
+  true
+end
+
+
+# Examples
+
+# p every?([3, 1, 11, 5]) { |n| n.even? }                                 # false
+# p every?([2, 4, 4, 8]) { |n| n.even? }                                  # true
+# p every?([8, 2]) { |n| n.even? }                                        # true
+# p every?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' }  # false
+# p every?(['squash', 'pea', 'kale', 'potato']) { |str| str[0] == 'p' }   # false
+# p every?(['parsnip', 'potato', 'pea']) { |str| str[0] == 'p' }          # true
+
+#####################################################################################
