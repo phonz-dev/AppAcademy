9dd808d7c9ef5555039c5c8bb26bb25ef9a8c6db
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 17:15:36 +0800

    Implement #conjunct_select

--- 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
@@ -194,3 +194,28 @@ end
 # p uncompress('a2b4c1') # 'aabbbbc'
 # p uncompress('b1o2t1') # 'boot'
 # p uncompress('x3y1x2z4') # 'xxxyxxzzzz'
+
+##########################################################################################
+##########################################################################################
+
+# PHASE 2
+
+# conjunct_select
+# Write a method conjunct_select that accepts an array and one or more procs as arguments. 
+# The method should return a new array containing the elements that return true when passed 
+# into all of the given procs.
+
+def conjunct_select(arr, *procs)
+  arr.select { |el| procs.all? { |prc| prc.call(el) } }
+end
+
+# Examples
+
+# is_positive = Proc.new { |n| n > 0 }
+# is_odd = Proc.new { |n| n.odd? }
+# less_than_ten = Proc.new { |n| n < 10 }
+
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive) # [4, 8, 11, 7, 13]
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
+
