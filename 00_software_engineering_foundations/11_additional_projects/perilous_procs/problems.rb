522e3649face4ec20ac0579b230169c95803308f
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 14:23:15 +0800

    Implement #multi_map

--- 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
@@ -233,3 +233,26 @@ end
 # arr_4 = [1, 7, 3, 5 ]
 # filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
 # p arr_4 # [1, 7, 3, 5]
+
+#####################################################################################
+
+# multi_map
+# Write a method multi_map that accepts an array, an optional number (n), and a block as 
+# arguments. The method should return a new array where each element of the original array 
+# is repeatedly run through the block n times. If the number argument is not passed in, then 
+# the the elements should be run through the block once.
+
+def multi_map(arr, n = 1, &prc)
+  arr_dup = arr.dup
+  n.times { arr_dup.map!(&prc) }
+  arr_dup
+end
+
+# Examples
+
+# p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
+# p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'}   # ["pretty!", "cool!", "huh?!"]
+# p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'}   # ["pretty!!!", "cool!!!", "huh?!!!"]
+# p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
+# p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
+# p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             # [40000, 30000, 20000, 70000]
