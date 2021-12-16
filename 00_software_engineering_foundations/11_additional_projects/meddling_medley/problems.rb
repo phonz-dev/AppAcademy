b627c513e14ef2fe45a2f445509e10a19ffd1f65
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 17:12:36 +0800

    Implement #uncompress

--- 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/meddling_medley/problems.rb
@@ -166,3 +166,31 @@ end
 # p energetic_encoding('bike', {}) # '????'
 
 ##########################################################################################
+
+# uncompress
+# Write a method uncompress that accepts a string as an argument. The string will be formatted 
+# so every letter is followed by a number. The method should return an "uncompressed" version of 
+# the string where every letter is repeated multiple times given based on the number that appears 
+# directly after the letter.
+
+def uncompress(str)
+  uncompressed = ""
+
+  i = 1
+
+  while i < str.length
+    num = str[i].to_i
+    char = str[i - 1]
+    num.times { uncompressed += char }
+
+    i += 2
+  end
+
+  uncompressed
+end
+
+# Examples
+
+# p uncompress('a2b4c1') # 'aabbbbc'
+# p uncompress('b1o2t1') # 'boot'
+# p uncompress('x3y1x2z4') # 'xxxyxxzzzz'
