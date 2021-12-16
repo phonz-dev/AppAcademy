19c395c5f837d08cc2e8deb303886737504a32a7
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-16 10:06:09 +0800

    Implement #at_least?

--- 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
+++ 00_software_engineering_foundations/11_additional_projects/perilous_procs/problems.rb
@@ -65,3 +65,40 @@ end
 # p filter_out([1, 7, 3, 5 ]) { |x| x.even? }         # [1, 7, 3, 5]
 
 #####################################################################################
+
+# at_least?
+# Write a method at_least? that accepts an array, a number (n), and a block as an arguments. 
+# The method should return a boolean indicating whether or not at least n elements of the array 
+# return true when given to the block. Solve this using Array#each.
+
+def at_least?(arr, n, &prc)
+  count = 0
+  arr.each  { |el| count += 1 if prc.call(el) }
+  count >= n
+end
+
+# Examples
+
+# p at_least?(['sad', 'quick', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
+# # false
+# p at_least?(['sad', 'quickly', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
+# # false
+# p at_least?(['sad', 'quickly', 'timidly', 'final'], 2) { |s| s.end_with?('ly') }
+# # true
+# p at_least?(['sad', 'quickly', 'timidly', 'finally'], 2) { |s| s.end_with?('ly') }
+# # true
+# p at_least?(['sad', 'quickly', 'timid', 'final'], 1) { |s| s.end_with?('ly') }
+# # true
+# p at_least?(['sad', 'quick', 'timid', 'final'], 1) { |s| s.end_with?('ly') }
+# # false
+# p at_least?([false, false, false], 3) { |bool| bool }
+# # false
+# p at_least?([false, true, true], 3) { |bool| bool }
+# # false
+# p at_least?([true, true, true], 3) { |bool| bool }
+# # true
+# p at_least?([true, true, true, true], 3) { |bool| bool }
+# # true
+
+#####################################################################################
+
