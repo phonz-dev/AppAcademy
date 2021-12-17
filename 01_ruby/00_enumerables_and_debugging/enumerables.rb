b8ecc5ea90aedb8a6169db489d1b197f0706fe2b
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-17 21:20:49 +0800

    Implement #my_reject

--- 01_ruby/00_enumerables_and_debugging/enumerables.rb
+++ 01_ruby/00_enumerables_and_debugging/enumerables.rb
@@ -15,4 +15,10 @@ class Array
     self.my_each { |el| selected << el if prc.call(el) }
     selected
   end
+
+  def my_reject(&prc)
+    new_array = []
+    self.my_each { |el| new_array << el unless prc.call(el) }
+    new_array
+  end
 end
