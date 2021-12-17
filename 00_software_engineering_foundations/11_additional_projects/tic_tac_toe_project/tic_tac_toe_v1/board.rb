e969905633187d848c0a651d2ba36799aaa40a11
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-17 10:33:02 +0800

    Initial Board class version

---
+++ 00_software_engineering_foundations/11_additional_projects/tic_tac_toe_project/tic_tac_toe_v1/board.rb
@@ -0,0 +1,32 @@
+class Board
+  def initialize
+    @grid = Array.new(3) { Array.new(3, "_") }
+  end
+
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos, mark)
+    row, col = pos
+    @grid[row][col] = mark
+  end
+
+  def valid?(pos)
+    row, col = pos
+    (row < 3 && row >= 0) && (col < 3 && col >=0)
+  end
+
+  def empty?(pos)
+    self[pos] == "_"
+  end
+
+  def place_mark(pos, mark)
+    unless self.valid?(pos) && self.empty?(pos)
+      raise "invalid mark"
+    end
+
+    self[pos] = mark
+  end
+end
