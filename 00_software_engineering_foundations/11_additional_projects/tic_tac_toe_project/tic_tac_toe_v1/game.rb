fc6a54124f367d70d81b63a0ed03c5ef3e85bc52
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-17 13:59:16 +0800

    Add Game instance variables

---
+++ 00_software_engineering_foundations/11_additional_projects/tic_tac_toe_project/tic_tac_toe_v1/game.rb
@@ -0,0 +1,11 @@
+require_relative "./board.rb"
+require_relative "./human_player.rb"
+
+class Game
+  def initialize(player_1_mark, player_2_mark)
+    @player_1 = HumanPlayer.new(player_1_mark)
+    @player_2 = HumanPlayer.new(player_2_mark)
+    @board = Board.new
+    @current_player = @player_1
+  end
+end
