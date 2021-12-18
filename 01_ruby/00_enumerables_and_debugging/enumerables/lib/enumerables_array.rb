a3cd54e3bd94390716b8c8e604414d77bd70e2ab
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-18 09:03:18 +0800

    Implement #subwords

--- 01_ruby/00_enumerables_and_debugging/enumerables/lib/enumerables_array.rb
+++ 01_ruby/00_enumerables_and_debugging/enumerables/lib/enumerables_array.rb
@@ -7,3 +7,22 @@ def is_prime?(n)
   return false if n < 2
   (2...n).none? { |divisor| n % divisor == 0 }
 end
+
+def subwords(string, words)
+  substrings = get_substrings(string)
+  substrings
+            .select { |str| words.include?(str)}
+            .uniq
+end
+
+def get_substrings(string)
+  substrings = []
+
+  (0...string.length).each do |start_idx|
+    (start_idx...string.length).each do |end_idx|
+      substrings << string[start_idx..end_idx]
+    end
+  end
+
+  substrings
+end
