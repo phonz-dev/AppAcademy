df056238fb3f5f9bd0251ec1de43d3421d7e160b
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-18 13:30:43 +0800

    Add #current_player, #previous_player and #next_player!

--- 01_ruby/00_enumerables_and_debugging/ghost/ghost_game.rb
+++ 01_ruby/00_enumerables_and_debugging/ghost/ghost_game.rb
@@ -1,10 +1,34 @@
 require "set"
 
 class GhostGame
+  attr_reader :players
+
   def initialize(*players)
-    file = File.open("dicitionary.txt")
-    @dictionary = file.readlines.map(&:chomp).to_set
+    file = File.open("dictionary.txt")
+    @dictionary ||= file.readlines.map(&:chomp).to_set
     @players = players
     @fragment = ""
   end
+
+  def current_player
+    @players.first
+  end
+
+  def previous_player
+    @players.last
+  end
+
+  def next_player!
+    @players = @players.rotate!
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = GhostGame.new("Dave", "Sarah")
+
+  p game
+
+  # p game.current_player
+  # game.next_player!
+  # p game.current_player
 end
