ee81b1e6410cafd19e632d002a3ee55e38c94d06
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-20 19:11:40 +0800

    Implement #make_guess

--- 01_ruby/01_reference/memory_puzzle/game.rb
+++ 01_ruby/01_reference/memory_puzzle/game.rb
@@ -5,4 +5,25 @@ class Game
     @board = Board.new
     @previous_guess = nil
   end
+
+  def make_guess(guess)
+    if @previous_guess.nil?
+      @previous_guess = guess
+      @board.reveal(@previous_guess)
+      return
+    end
+
+    next_guess = guess
+
+    if @board[@previous_guess] == @board[next_guess]
+      @board.reveal(next_guess)
+      puts "It's a match!"
+    else
+      @board[@previous_guess].flip
+      @board[next_guess].flip
+      puts "Try again."
+    end
+    
+    @previous_guess = nil
+  end
 end
