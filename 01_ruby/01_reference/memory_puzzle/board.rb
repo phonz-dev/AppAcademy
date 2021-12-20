f84c0cc80fb92b8be8bb71d44b0bb5322361a931
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-20 10:43:00 +0800

    Add basic Board features

--- 01_ruby/01_reference/memory_puzzle/board.rb
+++ 01_ruby/01_reference/memory_puzzle/board.rb
@@ -1,3 +1,31 @@
-class Board
+require_relative "./card.rb"
 
+class Board
+  VALUES = ("A".."Z").to_a
+
+  def initialize
+    @grid = Array.new(4) { Array.new(4, Card.new) }
+  end
+
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos, value)
+    row, col = pos
+    @grid[row][col] = value
+  end
+
+  def board_positions
+    positions = []
+
+    (0...@grid.length).each do |row|
+      (0...@grid.length).each do |col|
+        positions << [row, col]
+      end
+    end
+
+    positions
+  end
 end
