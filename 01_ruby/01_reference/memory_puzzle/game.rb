b6f71df6f4396c624e9d107faa5e09dbe0b5c062
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-20 20:54:38 +0800

    Refactor

--- 01_ruby/01_reference/memory_puzzle/game.rb
+++ 01_ruby/01_reference/memory_puzzle/game.rb
@@ -15,48 +15,45 @@ class Game
       self.prompt
       pos = gets.chomp.split(",").map(&:to_i)
       self.make_guess(pos)
+      sleep(1.5)
     end
 
     puts "Congratulations! You found them all!"
   end
 
   private
+  def make_guess(current_guess)
+    return if @board[current_guess].face_up?
 
-  def make_guess(pos)
-    if @previous_guess.nil?
-      self.make_first_guess(pos)
-    else
-      self.make_second_guess(pos)
-    end
-  end
-
-  def make_first_guess(guess)
     system("clear")
-    @board.reveal(guess)
-    @previous_guess = guess
+    @board.reveal(current_guess)
     @board.render
-    sleep(1)
+
+    if self.first_guess?
+      @previous_guess = current_guess
+      return
+    end
+
+    self.parse_guesses(@previous_guess, current_guess)
+    self.reset_previous_guess
   end
 
-  def make_second_guess(guess)
-    return if @board[guess].face_up?
-
-    system("clear")
-
-    @board.reveal(guess)
-    @board.render 
-
-    if @board[@previous_guess] == @board[guess]
+  def parse_guesses(guess_1, guess_2)
+    if @board[guess_1] == @board[guess_2]
       puts "It's a match!"
-      sleep(1)
     else
       puts "Try again."
-      sleep(1)
-      @board[@previous_guess].flip
-      @board[guess].flip
+      @board[guess_1].flip
+      @board[guess_2].flip
     end
-    
+  end
+
+  def reset_previous_guess
     @previous_guess = nil
+  end
+
+  def first_guess?
+    @previous_guess.nil?
   end
 
   def prompt
