800e18de0da897d74201fc99c8764b094f6c8712
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-20 08:31:27 +0800

    Add Card features

--- 01_ruby/01_reference/memory_puzzle/card.rb
+++ 01_ruby/01_reference/memory_puzzle/card.rb
@@ -1,3 +1,27 @@
 class Card
+  attr_reader :value
 
+  def initialize(value)
+    @value, @face_up =  value, false
+  end
+
+  def flip
+    self.face_up? ? self.hide : self.reveal
+  end
+
+  def face_up?
+    @face_up
+  end
+
+  def reveal
+    @face_up = true
+  end
+
+  def hide
+    @face_up = false
+  end
+
+  def to_s
+    self.face_up? ? @value : " "
+  end
 end
