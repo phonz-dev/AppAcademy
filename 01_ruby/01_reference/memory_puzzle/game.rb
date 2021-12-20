dd05e299c08b747c52979b9d3a1a828205b940e9
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-20 20:16:55 +0800

    Refactor

--- 01_ruby/01_reference/memory_puzzle/game.rb
+++ 01_ruby/01_reference/memory_puzzle/game.rb
@@ -6,24 +6,60 @@ class Game
     @previous_guess = nil
   end
 
-  def make_guess(guess)
-    if @previous_guess.nil?
-      @previous_guess = guess
-      @board.reveal(@previous_guess)
-      return
+  def play
+    @board.populate
+    
+    until @board.won?
+      system("clear")
+      @board.render
+      self.prompt
+      pos = gets.chomp.split(",").map(&:to_i)
+      self.make_guess(pos)
     end
 
-    next_guess = guess
+    puts "Congratulations! You found them all!"
+  end
 
-    if @board[@previous_guess] == @board[next_guess]
-      @board.reveal(next_guess)
-      puts "It's a match!"
+  private
+
+  def make_guess(pos)
+    if @previous_guess.nil?
+      self.make_first_guess(pos)
     else
-      @board[@previous_guess].flip
-      @board[next_guess].flip
+      self.make_second_guess(pos)
+    end
+  end
+
+  def make_first_guess(guess)
+    system("clear")
+    @board.reveal(guess)
+    @previous_guess = guess
+    @board.render
+    sleep(1)
+  end
+
+  def make_second_guess(guess)
+    return if @board[guess].face_up?
+
+    system("clear")
+
+    @board.reveal(guess)
+    @board.render 
+
+    if @board[@previous_guess] == @board[guess]
+      puts "It's a match!"
+      sleep(1)
+    else
       puts "Try again."
+      sleep(1)
+      @board[@previous_guess].flip
+      @board[guess].flip
     end
     
     @previous_guess = nil
+  end
+
+  def prompt
+    print "Please enter the position of the card you'd like to flip (e.g., '2,3') \n> "
   end
 end
