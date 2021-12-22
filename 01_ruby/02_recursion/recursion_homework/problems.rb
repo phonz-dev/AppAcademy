ab97fc64cd70cc6e4ef705d6234aba209a7deb15
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-22 13:09:42 +0800

    Implement #ice_cream_shop

--- 01_ruby/02_recursion/recursion_homework/problems.rb
+++ 01_ruby/02_recursion/recursion_homework/problems.rb
@@ -47,3 +47,23 @@ end
 # p gamma_fnc(1)  # => returns 1
 # p gamma_fnc(4)  # => returns 6
 # p gamma_fnc(8)  # => returns 5040
+
+######################################################################################################
+
+# Exercise 4 - Ice Cream Shop
+# Write a function ice_cream_shop(flavors, favorite) that takes in an array of ice cream flavors available 
+# at the ice cream shop, as well as the user's favorite ice cream flavor. Recursively find out whether or 
+# not the shop offers their favorite flavor.
+
+def ice_cream_shop(flavors, favorite)
+  return false if flavors.empty?
+  return true if flavors.first == favorite
+  ice_cream_shop(flavors[1..-1], favorite)
+end
+
+# Test Cases
+# p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+# p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+# p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+# p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+# p ice_cream_shop([], 'honey lavender')  # => returns false
