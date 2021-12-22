de730b10462e474b10fddbb9bddc943caf460338
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-22 11:23:44 +0800

    Implement #sum_to

---
+++ 01_ruby/02_recursion/recursion_homework/problems.rb
@@ -0,0 +1,14 @@
+# Exercise 1 - sum_to
+# Write a function sum_to(n) that uses recursion to calculate the sum from 1 to n (inclusive of n).
+
+def sum_to(n)
+  return nil if n < 0
+  return 0 if n.zero?
+  n + sum_to(n - 1)
+end
+
+# Test Cases
+p sum_to(5)  # => returns 15
+p sum_to(1)  # => returns 1
+p sum_to(9)  # => returns 45
+p sum_to(-8)  # => returns nil
