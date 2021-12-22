aa1ff65bf41c3a5cd861a894d1546d7061820fd7
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-22 08:53:29 +0800

    Add #color, #to_s, #value

--- 01_ruby/01_reference/sudoku/tile.rb
+++ 01_ruby/01_reference/sudoku/tile.rb
@@ -1,7 +1,27 @@
+require "colorize"
+
 class Tile
+  attr_reader :value
+
   def initialize(value)
     @value = value
-    @given = value != 0
+    @given = value == 0 ? false : true
   end
 
+  def color
+    @given ? :magenta : :yellow
+  end
+
+  def to_s
+    @given ? @value.to_s.colorize(color) : " "
+  end
+
+  def value=(new_value)
+    if @given
+      puts "You cannot change a given a value."
+      sleep(2)
+    else
+      @value = new_value
+    end
+  end
 end
