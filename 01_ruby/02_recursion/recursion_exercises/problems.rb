6996ae56b14f32789ba6ff90d922880bc9793630
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-22 13:48:34 +0800

    Add recursion exercises folder

---
+++ 01_ruby/02_recursion/recursion_exercises/problems.rb
@@ -0,0 +1,20 @@
+# Write a recursive method, range, that takes a start and an end and returns 
+# an array of all numbers in that range, exclusive. For example, range(1, 5) 
+# should return [1, 2, 3, 4]. If end < start, you can return an empty array.
+
+
+# p range(1, 5) #=> [1, 2, 3, 4]
+
+#############################################################################
+
+# Write both a recursive and iterative version of sum of an array.
+
+
+# p recursive_sum_array([]) #=> 0
+# p recursive_sum_array([1, 2, 3, 4]) #=> 10
+
+# p iterative_sum_array([]) #=> 0
+# p iterative_sum_array([1, 2, 3, 4]) #=> 10
+
+#############################################################################
+
