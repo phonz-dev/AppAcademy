36dc4d0380c7fe98acd3aab5a9b82d68ce89a9d0
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-23 18:13:45 +0800

    Change @all_seen_words to a hash, modify #explore_current_words

--- 01_ruby/02_recursion/word_chains/word_chainer.rb
+++ 01_ruby/02_recursion/word_chains/word_chainer.rb
@@ -7,7 +7,7 @@ class WordChainer
 
   def run(source, target)
     @current_words = [source]
-    @all_seen_words = [source]
+    @all_seen_words = { source => nil }
 
     until @current_words.empty?
       explore_current_words
@@ -21,13 +21,15 @@ class WordChainer
       current_adjacent_words = adjacent_words(current_word)
 
       current_adjacent_words.each do |adjacent_word|
-        next if @all_seen_words.include?(adjacent_word)
+        next if @all_seen_words.has_key?(adjacent_word)
         new_current_words << adjacent_word
-        @all_seen_words << adjacent_word
+        @all_seen_words[adjacent_word] = current_word
       end
     end
 
-    print new_current_words
+    new_current_words.each do |word|
+      puts "#{word} => #{@all_seen_words[word]}"
+    end
     @current_words = new_current_words
   end
 
