def0f5c9f7d2c1b16b421a41c14f8aa0ba87ee48
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-25 18:16:02 +0800

    Implement #parent=

--- 01_ruby/04_data_structures/poly_tree_node/skeleton/lib/00_tree_node.rb
+++ 01_ruby/04_data_structures/poly_tree_node/skeleton/lib/00_tree_node.rb
@@ -1,7 +1,20 @@
 class PolyTreeNode
-  attr_reader :value, :parent, :children
+  attr_accessor :value, :parent, :children
 
   def initialize(value)
     @value, @parent, @children = value, nil, []
+  end
+
+  def parent=(new_parent)
+    parent.children.delete(self) if parent && new_parent
+    @parent = new_parent
+    return if !new_parent || parent.children.include?(self) 
+    parent.children << self
+  end
+
+  def inspect
+    { "value" => @value, 
+      "parent_value" => @parent.value 
+    }.inspect
   end
 end
