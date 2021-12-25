5f7bdd62a1115f206a0c567dcc43dee88c355171
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-25 18:28:00 +0800

    Refactor

--- 01_ruby/04_data_structures/poly_tree_node/skeleton/lib/00_tree_node.rb
+++ 01_ruby/04_data_structures/poly_tree_node/skeleton/lib/00_tree_node.rb
@@ -6,26 +6,30 @@ class PolyTreeNode
   end
 
   def parent=(new_parent)
-    parent.children.delete(self) if parent && new_parent
+    parent.remove_child(self) if parent && new_parent
     @parent = new_parent
-    return if !new_parent || parent.children.include?(self) 
+    return if !new_parent || parent.is_child?(self) 
     parent.children << self
   end
 
   def add_child(new_child)
-    unless children.include?(new_child)
+    unless is_child?(new_child)
       children << new_child
       new_child.parent = self
     end
   end
 
   def remove_child(child)
-    unless children.include?(child)
+    unless is_child?(child)
       raise "child node does not exist"
     end
 
     child.parent = nil
     children.delete(child)
+  end
+
+  def is_child?(node)
+    children.include?(node)
   end
 
   def inspect
