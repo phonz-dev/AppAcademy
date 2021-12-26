59305382f40e2edc87fe5ccf5cb0457c05978a70
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-26 19:46:47 +0800

    Implement #move

--- 01_ruby/04_data_structures/tic_tac_toe_ai/skeleton/lib/super_computer_player.rb
+++ 01_ruby/04_data_structures/tic_tac_toe_ai/skeleton/lib/super_computer_player.rb
@@ -2,6 +2,21 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+
+    node.children.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+
+    node.children.each do |child|
+      unless child.losing_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+
+    raise "unwinnable"
   end
 end
 
