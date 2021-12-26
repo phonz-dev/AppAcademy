ae8c0bdabd610e24b33a4e2d2d2c0251db8b84ef
Alphonzo Escolar <phonz.dev@gmail.com>
2021-12-26 12:02:32 +0800

    Refactor

--- 01_ruby/04_data_structures/knights_travails/knight_path_finder.rb
+++ 01_ruby/04_data_structures/knights_travails/knight_path_finder.rb
@@ -15,24 +15,30 @@ class KnightPathFinder
   BOUNDARY = [0, 7]
 
   def self.valid_moves(pos)
-    row, col = pos
+    curr_x, curr_y = pos
 
-    moves = DELTAS.map { |(dx, dy)| [row + dx, col + dy] }
+    moves = DELTAS.map { |(dx, dy)| [curr_x + dx, curr_y + dy] }
     moves.select do |move|
       move.all? { |coord| coord.between?(BOUNDARY.first, BOUNDARY.last) }
     end
   end
 
   def initialize(start_pos)
-    @start_pos = PolyTreeNode.new(start_pos)
+    @root_node = PolyTreeNode.new(start_pos)
     @considered_positions = [ start_pos ]
     build_move_tree
   end
 
   def find_path(end_pos)
-    curr_node = @start_pos.dfs(end_pos)
+    curr_node = root_node.dfs(end_pos)
     trace_path_back(curr_node)
   end
+
+  private_constant :DELTAS
+
+  private
+
+  attr_reader :root_node, :considered_positions
 
   def trace_path_back(end_pos)
     curr_node = end_pos
@@ -49,14 +55,14 @@ class KnightPathFinder
   def new_move_positions(pos)
     positions = KnightPathFinder.valid_moves(pos)
     new_move_positions = positions.select do  |pos|
-      !@considered_positions.include?(pos)
+      !considered_positions.include?(pos)
     end
-    @considered_positions.concat(new_move_positions)
+    considered_positions.concat(new_move_positions)
     new_move_positions
   end
 
   def build_move_tree
-    tree = [ @start_pos ]
+    tree = [ root_node ]
 
     until tree.empty?
       curr_node = tree.shift
@@ -73,5 +79,5 @@ class KnightPathFinder
   end
 end
 
-# kpf = KnightPathFinder.new([0, 0])
-# p kpf.find_path([6, 2])
+kpf = KnightPathFinder.new([0, 0])
+p kpf.find_path([7, 7])
